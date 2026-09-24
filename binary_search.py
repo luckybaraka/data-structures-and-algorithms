698d5a6368235589c53b9c1d876eda4771f70682
Lucky Baraka <luckybaraka21@gmail.com>
2026-09-24 14:25:48 +0300

    added binary search where we are diving the things, the time complexity is O(logn)

---
+++ binary_search.py
@@ -0,0 +1,20 @@
+def binary_search(arr: list[int], item: int) -> int:
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+
+        if guess == item:
+            return mid
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+
+arr = [2, 4, 5, 6, 7, 8, 9, 10]
+res = binary_search(arr, 9)
+print(res)
+
